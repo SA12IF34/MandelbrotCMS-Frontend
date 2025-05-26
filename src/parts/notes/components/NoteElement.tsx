64b95308823824dd3bc0f038e2b98daafcee1ddd
Saif <ayeshsaif367@gmail.com>
2025-05-26 05:54:10 +0300

    some UI modifications

--- src/parts/notes/components/NoteElement.tsx
+++ src/parts/notes/components/NoteElement.tsx
@@ -10,7 +10,7 @@ type props = {
 function NoteElement({noteId, title, content, createDate}: props) {
   return (
     <Link to={`/notes/${noteId}`} className='note-element'>
-        <span>{createDate}</span>
+        <span>{createDate.split('T')[0]}</span>
         {title ? <h2>{title.length > 30 ? title.slice(0, 31)+'...' : title}</h2>
         :<h2>{content.length > 30 ? content.slice(0, 31)+'...': content}</h2>}
     </Link>
